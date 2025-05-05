53b4e2da2566afe3826741e15c8a7d7ad8d39fd1
NizarNaser <nizarnaser17@gmail.com>
2025-05-05 21:08:28 +0200

    first commit9

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/deluxe-auto', // قم بتغيير هذا إلى اسم مستودع GitHub الخاص بك
+  build: {
+    outDir: 'dist',
+  },
 });
+
 
 
 
